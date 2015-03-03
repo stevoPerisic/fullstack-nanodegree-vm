e77fde59a791b1ecc1b7ac285a097e23705ab6aa
stevoperisic <stevoperisic@Stevos-MacBook-Pro.local>
2015-03-02 23:07:21 -0500

    tournament sql file done with part 1
    created basic schema in tournament.sql

---
+++ vagrant/tournament/README.md
@@ -0,0 +1,18 @@
+## Project 2 - Swiss Tournament Planner
+
+This is a Python module that uses the PostgreSQL database to keep track
+of players and matches in a game tournament.
+
+The game tournament will use the Swiss system for pairing up players in 
+each round: players are not eliminated, and each player should be paired with another player with teh samenumber of wins, or as close as possible.
+
+This project has two parts: defining the database schema (SQL table definitions), and writing the code that will use it.
+
+## Requirements
+
+to do
+
+## Installation and running
+
+
+
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,46 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE DATABASE tournament;
+
+\c tournament;
+
+CREATE TABLE Players (
+	id SERIAL PRIMARY KEY,
+	name varchar(40)  
+);
+
+CREATE TABLE Matches (
+	id SERIAL PRIMARY KEY,
+	p1 varchar(40),
+	p2 varchar(40),
+	winner integer REFERENCES Players(id)
+);
+
+INSERT INTO Players (name) VALUES ('Stevo'), ('Bojana');
+
+INSERT INTO Matches (p1, p2, winner) 
+VALUES ('Stevo', 'Bojana', 2), ('Bojana', 'Stevo', 1),('Stevo', 'Bojana', 1);
+
+CREATE VIEW getAllMatches_byPlayer AS
+	SELECT name, count(*) as matches_played
+	FROM Players
+	LEFT JOIN Matches 
+	ON Players.name = Matches.p1 OR Players.name = Matches.p2
+	GROUP BY Players.name;
+
+CREATE VIEW getAllWins_byPlayer AS
+	SELECT name, count(*) as wins
+	FROM Players
+	LEFT JOIN Matches 
+	ON Players.id = Matches.winner
+	GROUP BY Players.name;
+
+CREATE VIEW getStandings AS
+	SELECT name, count(*) as wins
+	FROM Players
+	LEFT JOIN Matches 
+	ON Players.id = Matches.winner
+	GROUP BY Players.name
+	ORDER BY wins DESC;
 
