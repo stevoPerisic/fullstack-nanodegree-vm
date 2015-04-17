079cf555b079db3e3ddda9ccbaaa79892e6122ef
stevoperisic <stevoperisic@MacBook-Pro.local>
2015-04-17 00:17:27 -0400

    up to step 5

--- vagrant/project3/project.py
+++ vagrant/project3/project.py
@@ -5,6 +5,19 @@ from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
+#NEW IMPORTS FOR OAUTH
+from flask import session as login_session
+import random, string
+
+# IMPORTS FOR SERVER REDIRECT
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+CLIENT_ID = json.loads(open('client_secret.json', 'r').read())['web']['client_id']
 
 #Connect to Database and create database session
 engine = create_engine('sqlite:///restaurantmenu.db')
@@ -136,6 +149,99 @@ def deleteMenuItem(restaurant_id,menu_id):
     else:
         return render_template('deleteMenuItem.html', item = itemToDelete)
 
+
+# login
+# Create a steate token to prevent request forgery
+# Store it in a session for later validation
+@app.route('/login')
+def showLogin():
+    state  = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    login_session['state'] = state
+    # RENDER THE LOGIN TEMPLATE
+    return render_template('login.html', STATE = login_session['state'])
+    # return "The current session state is %s" % login_session['state']
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    if request.args.get('state') != login_session['state']:
+      response = make_response(json.dumps('Invalid state parameter'), 401)
+      response.headers['Content-Type'] = 'application/json'
+      return response
+
+    code = request.data
+
+    try:
+      # Upgrade the authorization code into a credentials object 
+      oauth_flow = flow_from_clientsecrets('client_secret.json', scope='')
+      oauth_flow.redirect_uri = 'postmessage'
+      credentials  = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+      reponse = make_response(json.dumps('failed to upgrade the authorization code'), 401)
+      response.headers['Content-Type'] = 'application/json'
+      return response
+
+    # check that the access token is valid
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+
+    # if there was an error in the access token, abort.
+    if result.get('error') is not None:
+      response = make_response(json.dumps(result.get('error')), 500)
+      response.headers['Content-Type'] = 'application/json'
+      return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+      response = make_response(
+          json.dumps("Token's user ID doesn't match given user ID."), 401)
+      response.headers['Content-Type'] = 'application/json'
+      return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+      response = make_response(
+          json.dumps("Token's client ID does not match app's."), 401)
+      print "Token's client ID does not match app's."
+      response.headers['Content-Type'] = 'application/json'
+      return response
+
+    # check if the user is alreadu lgged in
+    stored_credentials = login_session.get('credentials')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+      response.make_response(json.dumps('Current User is already connected'), 200)
+      response.headers['Content-Type'] = 'application/json'
+
+    # Store the access token in the sesion for later use
+    login_session['credentials'] = credentials
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = 'https://www.googleapis.com/oauth2/v1/userinfo'
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    #login_session['email'] = data['email']
+    
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += '" style = "width: 300px; heigth: 300px; border-radius: 150px;">'
+
+    flash('you are now logged in as %s' % login_session['username'])
+
+    return output
 
 
 
